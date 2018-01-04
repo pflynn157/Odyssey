63646e559a97cfd4f247f2d34c4e26a0d2921ddc
patrickf2000 <patrick_dev2000@outlook.com>
2018-01-04 16:10:32 -0500

    added advanced paste support for copying files

--- src/actions.cxx
+++ src/actions.cxx
@@ -1,7 +1,6 @@
 #include <QFile>
 #include <QDir>
 #include <QMessageBox>
-#include <iostream>
 
 #include "actions.hh"
 #include "clipboard.hh"
@@ -24,23 +23,15 @@ void Actions::paste() {
         bool ret = QFile(oldAddr).rename(newAddr);
         if (ret==false) {
             if (QFileInfo(newAddr).exists()) {
-                QMessageBox msg;
-                msg.setWindowTitle("Warning");
-                msg.setText("A file with the same name already exists. "
-                            "Do you wish to overwrite it?");
-                msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-                msg.setIcon(QMessageBox::Question);
-                int ret = msg.exec();
-                if (ret==QMessageBox::Yes) {
-                    if (QFileInfo(newAddr).isDir()) {
-                        QDir(newAddr).removeRecursively();
-                    } else {
-                        QFile(newAddr).remove();
-                    }
-                } else {
-                    newAddr = clipboard.newPath+"(copy) "+clipboard.fileName;
+                newAddr = handleDuplicate(newAddr);
+                ret = QFile(oldAddr).rename(newAddr);
+                if (ret==false) {
+                    QMessageBox msg;
+                    msg.setWindowTitle("Fatal Error");
+                    msg.setText("An unknown critical error has occurred.");
+                    msg.setIcon(QMessageBox::Critical);
+                    msg.exec();
                 }
-                QFile(oldAddr).rename(newAddr);
             } else {
                 QMessageBox msg;
                 msg.setWindowTitle("Error");
@@ -53,22 +44,35 @@ void Actions::paste() {
             }
         }
     } else {
+        bool ret;
         if (clipboard.filetype==Clipboard_FileType::TYPE_FILE) {
-            bool ret = QFile(oldAddr).copy(newAddr);
-            if (ret==false) {
-                QMessageBox msg;
-                msg.setWindowTitle("Error");
-                msg.setText("There was an error copying this file.");
-                msg.setIcon(QMessageBox::Warning);
-                msg.exec();
-            }
+            ret = QFile(oldAddr).copy(newAddr);
         } else {
-            bool ret = copyDirectory(oldAddr,newAddr);
-            if (ret==false) {
+            ret = copyDirectory(oldAddr,newAddr);
+        }
+        if (ret==false) {
+            if (QFileInfo(newAddr).exists()) {
+                newAddr = handleDuplicate(newAddr);
+                if (clipboard.filetype==Clipboard_FileType::TYPE_FILE) {
+                    ret = QFile(oldAddr).copy(newAddr);
+                } else {
+                    ret = copyDirectory(oldAddr,newAddr);
+                }
+                if (ret==false) {
+                    QMessageBox msg;
+                    msg.setWindowTitle("Fatal Error");
+                    msg.setText("An unknown critical error has occurred.");
+                    msg.setIcon(QMessageBox::Critical);
+                    msg.exec();
+                }
+            } else {
                 QMessageBox msg;
                 msg.setWindowTitle("Error");
-                msg.setText("There was an error copying this folder.");
-                msg.setIcon(QMessageBox::Warning);
+                msg.setIcon(QMessageBox::Critical);
+                msg.setText("There was an error moving this file!");
+                msg.setDetailedText("This could mean that the file is read-only, "
+                                    "or that you do not have the proper permissions "
+                                    "to access it.");
                 msg.exec();
             }
         }
@@ -77,6 +81,26 @@ void Actions::paste() {
     if (clipboard.action==Clipboard_Actions::CUT) {
         clipboard.action = Clipboard_Actions::NONE;
     }
+}
+
+QString Actions::handleDuplicate(QString newAddr) {
+    QMessageBox msg;
+    msg.setWindowTitle("Warning");
+    msg.setText("A file with the same name already exists. "
+                "Do you wish to overwrite it?");
+    msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msg.setIcon(QMessageBox::Question);
+    int ret = msg.exec();
+    if (ret==QMessageBox::Yes) {
+        if (QFileInfo(newAddr).isDir()) {
+            QDir(newAddr).removeRecursively();
+        } else {
+            QFile(newAddr).remove();
+        }
+    } else {
+        newAddr = clipboard.newPath+"(copy) "+clipboard.fileName;
+    }
+    return newAddr;
 }
 
 bool Actions::copyDirectory(QString oldPath, QString newPath) {
--- src/actions.hh
+++ src/actions.hh
@@ -6,5 +6,6 @@ class Actions {
 public:
     static void paste();
 private:
+    static QString handleDuplicate(QString newAddr);
     static bool copyDirectory(QString oldPath, QString newPath);
 };
